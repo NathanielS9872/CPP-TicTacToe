985768b78cbbaa10aeec7d35141f9fd23182f656
Nathaniel Springer <500189872@fvtc.edu>
2025-03-20 19:27:14 -0500

    Added comments for Peer Review

--- CPP-TicTacToe/TicTacToe.cpp
+++ CPP-TicTacToe/TicTacToe.cpp
@@ -3,10 +3,10 @@
 bool TicTacToe::IsGameOver() const{
 	if (CheckLines(0, 1, 2) || CheckLines(3, 4, 5) || CheckLines(6, 7, 8) || CheckLines(0, 3, 6) || CheckLines(1, 4, 7) || CheckLines(2, 5, 8) || CheckLines(0, 4, 8) || CheckLines(2, 4, 6)) {
 		return true;
-	}
+	}//Uses CheckLines() to determine if there is 3 in a row vertically, horizontally, or diagonally anywhere, indicating a completed game
 	else {
-		if (GetCount() > 9) { return true; }
-		return false;
+		if (GetCount() > 9) { return true; }//Returns true if the turn count is 9, indicating a filled board and completed game
+		return false;//Returns false if it does not return true
 	}
 }
 void TicTacToe::TakeTurn() {
@@ -14,26 +14,26 @@ void TicTacToe::TakeTurn() {
 	bool loop = true;
 	std::string board = GetBoard();
 	std::cout << "Where would you like to go? (1-9): ";
-	while (loop) {
+	while (loop) {//loops until the input is 1-9, and in a spot not marked by X or O (defaults to the position number)
 		std::cin >> input;
 		if (std::stoi(input) > 0 && std::stoi(input) < 10) {
 			if (board[stoi(input) - 1] != 'X'&&board[stoi(input)-1] != 'O') {
-				board[stoi(input) - 1] = GetTurn();
+				board[stoi(input) - 1] = GetTurn();//Ends loop and changes appropriate index to X or O depending on the current turn
 				loop = false;
 			}
 			else {
 				Display();
-				std::cout << "Spot already taken.\nWhere would you like to go? (1-9): ";
+				std::cout << "Spot already taken.\nWhere would you like to go? (1-9): ";//Redisplays board and error message if spot is taken
 			}
 		}
 		else {
 			Display();
-			std::cout << "Invalid number.\nWhere would you like to go ? (1-9) : ";
+			std::cout << "Invalid number.\nWhere would you like to go ? (1-9) : ";//Redisplays board and error message if the number is not 1-9
 		}
 	}
-	SetBoard(board);
-	GetTurn() == 'X' ? SetTurn('O') : SetTurn('X');
-	SetCount(GetCount() + 1);
+	SetBoard(board);//Updates m_board
+	GetTurn() == 'X' ? SetTurn('O') : SetTurn('X');//Changes turn
+	SetCount(GetCount() + 1);//Increases turn count by 1
 }
 //1,2,3  (0,1,2
 //4,5,6   3,4,5
@@ -42,17 +42,17 @@ void TicTacToe::Display() const{
 	system("cls");
 	std::string board = GetBoard();
 	if (CheckLines(0, 1, 2) || CheckLines(3, 4, 5) || CheckLines(6, 7, 8) || CheckLines(0, 3, 6) || CheckLines(1, 4, 7) || CheckLines(2, 5, 8) || CheckLines(0, 4, 8) || CheckLines(2, 4, 6)) {
-		std::cout<<"\n\n" << board[0] << "|" << board[1] << "|" << board[2] << "\n------\n" << board[3] << "|" << board[4] << "|" << board[5] << "\n------\n" << board[6] << "|" << board[7] << "|" << board[8] << "\n\n" << GetTurn() == 'X' ? 'O':'X' << " has won the game!\n";
-	}
+		std::cout<<"\n\n " << board[0] << "|" << board[1] << "|" << board[2] << "\n ------\n " << board[3] << "|" << board[4] << "|" << board[5] << "\n ------\n " << board[6] << "|" << board[7] << "|" << board[8] << "\n\n" << (GetTurn() == 'X' ? 'O':'X') << " has won the game!\n";
+	}//Displays board state and the winner if CheckLines returns true with any line combination
 	else if(GetCount() > 9){
-		std::cout <<"\n\n" << board[0] << "|" << board[1] << "|" << board[2] << "\n------\n" << board[3] << "|" << board[4] << "|" << board[5] << "\n------\n" << board[6] << "|" << board[7] << "|" << board[8] << "\n\nThe game has ended in a Draw!\n";
-	}
+		std::cout <<"\n\n " << board[0] << "|" << board[1] << "|" << board[2] << "\n ------\n " << board[3] << "|" << board[4] << "|" << board[5] << "\n ------\n " << board[6] << "|" << board[7] << "|" << board[8] << "\n\nThe game has ended in a Draw!\n";
+	}//Displays board state and declares the match a draw if board is filled (done after checking for winners)
 	else {
-		std::cout << "Turn " << GetCount() << " (" << GetTurn() << "'s turn):\n\n" << board[0] << "|" << board[1] << "|" << board[2] << "\n------\n" << board[3] << "|" << board[4] << "|" << board[5] << "\n------\n" << board[6] << "|" << board[7] << "|" << board[8] << "\n\n";
-	}
+		std::cout << "Turn " << GetCount() << " (" << GetTurn() << "'s turn):\n\n " << board[0] << "|" << board[1] << "|" << board[2] << "\n ------\n " << board[3] << "|" << board[4] << "|" << board[5] << "\n ------\n " << board[6] << "|" << board[7] << "|" << board[8] << "\n\n";
+	}//Displays board state in preperation for TakeTurn()
 }
 bool TicTacToe::CheckLines(const int num1,const int num2, const int num3) const{
 	std::string board = GetBoard();
 	if (board[num1] == board[num2] && board[num2] == board[num3]) { return true; }
-	return false;
+	return false;//returns true if 3 designated locations in the board are the same
 }
--- CPP-TicTacToe/TicTacToe.h
+++ CPP-TicTacToe/TicTacToe.h
@@ -3,15 +3,15 @@
 #include <string>
 class TicTacToe : public Game {
 private:
-	std::string m_board = "123456789";
-	char m_turn = 'X';
-	int m_turnCount = 1;
+	std::string m_board = "123456789";//Tracks board state. had some troubles making an array, so just made a string since that is a character array
+	char m_turn = 'X';//Tracks current turn starting at X
+	int m_turnCount = 1;//Tracks turn count
 public:
 	bool IsGameOver() const;
 	void TakeTurn();
 	void Display() const;
 	bool CheckLines(const int num1, const int num2, const int num3) const;
-	void SetBoard(const std::string board) { m_board = board; }
+	void SetBoard(const std::string board) { m_board = board; }//Start of Getters/Setters
 	std::string GetBoard() const { return m_board; }
 	char GetTurn() const { return m_turn; }
 	void SetTurn(const char turn) { m_turn = turn; }
